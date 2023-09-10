80284caad3f95f4dc69ef9a473f707b78a067196
niwaniwa <niwaniwa@users.noreply.github.com>
2023-09-10 18:02:02 +0900

    impl hugo build sh

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 
 set -eu
 
-mkdir dist
+mkdir public
 
-# cp -r static/* dist
-cp vpm.json dist/
+src="vpm.niri.la/index_profile.html"
+dst="vpm.niri.la/themes/PaperMod/layouts/partials/index_profile.html"
+
+cp "$src" "$dst"
+
+hugo
+
+cp -r vpm.niri.la/public/* public
+cp vpm.json public/content/
